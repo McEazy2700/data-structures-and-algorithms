056d1eaae49dd4e9a24fb57e77d44c20416d02bf
Ezekiel Victor <codeepoch@gmail.com>
2022-09-11 16:08:01 +0000

    added to linked list class

--- linked-list.py
+++ linked-list.py
@@ -4,9 +4,18 @@ class Node:
         self.next = next
 
 class SLinkedList:
-    def __init__(self, head:Node):
+    def __init__(self, head:Node=None):
         self.head = head
 
-list1 = SLinkedList(Node('First'))
+    def print_list(self):
+        node = self.head
 
-print(list1.head.value)
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+list1 = SLinkedList(Node('First'))
+n2 = Node('node 2')
+list1.head.next = n2
+
+list1.print_list()
